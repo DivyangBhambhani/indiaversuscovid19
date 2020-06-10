46b0a08229e4eba3ccad7536a06b2b6c5fd28e80
Divyang Bhambhani <divyang.bhambhani@thepsi.com>
2020-06-10 20:07:41 +0530

    Added Google Analytics

--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ import Zones from './components/zones/index';
 import Statistics from './components/statistics/index';
 import Analytics from './components/analytics/index';
 import LatestUpdates from './components/latest_updates/index';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-168120519-1');
 
 function App() {
-  return (
-    <div className="App">
-    	<BrowserRouter>
-            <Switch>
-                <Route exact path = '/' component={Dashboard} />
-                <Route exact path = '/analytics' component={Analytics} />
-                <Route exact path = '/zones' component={Zones} />
-                <Route exact path = '/statistics' component={Statistics} />
-                <Route exact path = '/updates' component={LatestUpdates} />
-                <Route render = {() => (<Header>Not Found</Header>) } />
-            </Switch>
-        </BrowserRouter>
-    </div>
-  );
+    React.useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search); 
+    });
+    return (
+        <div className="App">
+        	<BrowserRouter>
+                <Switch>
+                    <Route exact path = '/' component={Dashboard} />
+                    <Route exact path = '/analytics' component={Analytics} />
+                    <Route exact path = '/zones' component={Zones} />
+                    <Route exact path = '/statistics' component={Statistics} />
+                    <Route exact path = '/updates' component={LatestUpdates} />
+                    <Route render = {() => (<Header>Not Found</Header>) } />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
